a43289f8b6cc8919a3f6c1b8e7bcf2da670cd623
Joaquin <joaquin.higuitalo@amigo.edu.co>
2026-05-13 19:53:10 -0500

    mas caoacidad para imagenes y ruta

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,8 +37,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 app.get("/", (req, res) => {
   res.send("PetCare API funcionando");
