80d10ecec48560e3934fda438c354390f1384084
Joaquin <joaquin.higuitalo@amigo.edu.co>
2026-05-13 19:03:22 -0500

    agregar preview vercel cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://petcare-jade.vercel.app",
   "https://petcare-5qvs83v0-joaquinhiguitalo-projects.vercel.app",
-  "https://pet-care-mvp-phi.vercel.app"
+  "https://pet-care-mvp-phi.vercel.app",
+  "https://pet-care-q17xfaasy-joaquinhiguitalos-projects.vercel.app"
 ];
 
 app.use(cors({
